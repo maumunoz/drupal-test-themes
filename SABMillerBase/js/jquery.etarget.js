e4302be45a8ad24b0f94f145db60047c604ffadf
maumunoz <maumunoz@gmail.com>
2014-11-17 13:54:34 -0600

    Adding Init Module and Transform

--- SABMillerBase/js/jquery.etarget.js
+++ SABMillerBase/js/jquery.etarget.js
@@ -1,9 +1,13 @@
 (function ($, jq) {
-    $.fn.etarget = function (type, destination) {
+    $.fn.etarget = function (type, destination, filter) {
         this.on(type, function (e) {
+            var target = jq(destination);
             e.preventDefault();
             e.stopPropagation();
-            jq(destination).trigger(type);
+            if (filter) {
+                target = filter(e, target, jq);
+            }
+            target.trigger(type);
         });
 
         return this; // make it chainable
