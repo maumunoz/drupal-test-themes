4d559dc75e02d4ffee5e2a6c6b311eb7c6ea245a
Roberto Ramos Vargas <roberto.ramos@accenture.com>
2014-11-25 17:44:59 -0600

    Change in the selector target in transform function Styles for the header and

--- aguila_theme/js/global.js
+++ aguila_theme/js/global.js
@@ -1,3 +1,21 @@
 (function ($) {
+    $(document).ready(function () {
+        $.transform("#block-views-formulario-aguila-news-block-1", {
+            target: "#web-form-aguila-news-container",
+            type: 'append'
+        });
+
+        //Transform Logo
+        transform_logo();
+    });
+
+    function transform_logo () {
+        var node = $("#block-system-main-menu .menu li:eq(2)").after('<li class=" leaf item-logo"></li>');
+
+        $.transform(".site-name-wrapper", {
+            target: "#block-system-main-menu .menu .item-logo",
+            type: 'append'
+        });
+    }
 
 })(jQuery2);
