bf4f7db936b11a620afa842c160b9664fd5680e5
jose.montero <jose.montero@accenture.com>
2014-11-25 17:51:03 -0600

    Aguila theme - Footer basic styles

--- aguila_theme/js/global.js
+++ aguila_theme/js/global.js
@@ -1,3 +1,28 @@
+WebFontConfig = {
+    google: {
+        families: ['Open+Sans:300italic,400italic,600italic,700,300,600,400:latin']
+    }
+};
+
+(function() {
+    var wf = document.createElement('script');
+    wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+        '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+    wf.type = 'text/javascript';
+    wf.async = 'true';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(wf, s);
+})();
+
 (function ($) {
 
+$(document).ready(function () {
+    // Transform Newsletter Form
+    $.transform("#block-views-formulario-aguila-news-block-1", {
+        target: "#web-form-aguila-news-container",
+        type: 'append'
+    });
+});
+
 })(jQuery2);
+
