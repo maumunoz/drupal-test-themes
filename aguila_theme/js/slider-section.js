f88008cfdf871a39a5c62f11be8e742cbb64f35e
jose.montero <jose.montero@accenture.com>
2014-12-02 17:39:08 -0600

    Aguila theme - Slider dots styles, Footer styles updates and Open Sans and Roboto Google web fonts

--- aguila_theme/js/slider-section.js
+++ aguila_theme/js/slider-section.js
@@ -14,6 +14,7 @@
         //Initialize Slick slider
         slider.slick({
             dots: true,
+            arrows: false,
             infinite: true,
             speed: 300,
             slidesToShow: 1,
---
+++ aguila_theme/js/webfont.js
@@ -0,0 +1,16 @@
+/*global WebFontConfig */
+window.WebFontConfig = {
+    google: {
+        families: ['Roboto:400,700:latin', 'Open+Sans:400italic,400,300,700,600,800:latin']
+    }
+};
+
+(function() {
+    var wf = document.createElement('script');
+    wf.src = ('https:' === document.location.protocol ? 'https' : 'http') +
+        '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+    wf.type = 'text/javascript';
+    wf.async = 'true';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(wf, s);
+}());
