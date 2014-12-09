2db4855ad4cf88c66a52149ed33f1e9900bef231
Esteban <matatron@gmail.com>
2014-12-09 15:00:43 -0600

    Aguila theme - eventos mapa - add styles and functionality

---
+++ aguila_theme/js/eventos-mapa.js
@@ -0,0 +1,75 @@
+/*global jQuery2 */
+(function ($) {
+
+    var map;
+    var markers = [];
+    var centerLatlng;
+    
+    function validGeolocation(value) {
+        return value !== undefined && value !== null && value !== '';
+    }
+    
+    function centerMap() {
+        var latlngbounds = new google.maps.LatLngBounds();
+        $(markers).each(function(i,data) {
+           latlngbounds.extend(data.marker.getPosition());
+        });
+        centerLatlng = latlngbounds.getCenter();
+        map.setCenter(centerLatlng);
+        map.fitBounds(latlngbounds); 
+    }
+
+    $.initModule('.mapa-eventos', function ($) {
+        $.mapsLoaded.promise().then(function () {
+        
+            mapOptions = {
+                zoom: 6,
+                styles: $.mapStyles.grayscale,
+                center: new google.maps.LatLng(4.6194477,-74.110567),
+                draggable: ($(document).width() > 480),
+                scrollwheel: false,
+            },
+            map = new google.maps.Map($('.mapa-eventos')[0], mapOptions);
+    
+            var linksWithCoords = $('.eventos-aguila-en-vivo').find('.evento');
+            linksWithCoords.each(function(i, node) {
+                node = $(node);
+                if (validGeolocation(node.data('latitude')) && validGeolocation(node.data('longitude'))) {
+                    var position = new google.maps.LatLng(parseFloat(node.data('latitude')),parseFloat(node.data('longitude')));
+                    var marker = new google.maps.Marker({
+                        position: position,
+                        title: node.data('title')
+                    });
+                    infowindow = new google.maps.InfoWindow({
+                        content: node.html()
+                    });
+                    markers.push({marker:marker, infowindow: infowindow});
+                    marker.setMap(map);
+                    google.maps.event.addListener(marker, 'click', function () {
+                        $(markers).each(function(j,alldata) { alldata.infowindow.close(); });
+                        infowindow.open(map, marker);
+                    });
+                }else{
+                    node.addClass("hidden");
+                }
+            });
+            if (markers.length>0) {
+                $('.view-map-all').bind("click",function(){
+                    map.setZoom(6);
+                    $(markers).each(function(j,alldata) { alldata.infowindow.close(); });
+                    map.panTo(centerLatlng);
+                });
+                google.maps.event.addListener(map, 'click', function () {
+                    map.setOptions({
+                        scrollwheel: true,
+                    });
+                });
+                google.maps.event.addDomListener(window, "resize", function() {
+                    centerMap();
+                });
+                centerMap();            
+            }
+        
+        });
+    });
+}(jQuery2));
