12bb390c302dd95bd3167d572c0aa0d59d8cbe31
Nefi Urena <nefi.e.urena.salas@accenture.com>
2014-12-22 20:13:04 -0600

    Cerveza Aguila - Banner Cerveza Aguila - Web Styling a la sección de banner principal.

--- aguila_theme/js/banner-cervezas.js
+++ aguila_theme/js/banner-cervezas.js
@@ -3,11 +3,17 @@
     var node = '.banner-cerveza';
 
     $.initModule(node, function ($, el) {
-        var bgNode = el.find('.views-field.views-field-field-cerveza-imagen-fondo');
+        var bgNode = el.find('.views-field.views-field-field-cerveza-imagen-fondo'),
+        	shareLinks = el.find('.compartir a');
         if($(bgNode).length) {
             el.css('backgroundImage', 'url(' + bgNode.find('img').attr('src') + ')');
             bgNode.find('img').remove();
         }
+
+        shareLinks.on('click', function (e) {
+            e.preventDefault();
+            window.open($(this).attr('href'), '_blank', 'width=600,height=350');
+        });
     });
 
 }(jQuery2));
