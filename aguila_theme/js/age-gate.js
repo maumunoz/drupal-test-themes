be5db7a7690d09696b3d111afa456adda130c365
Esteban <matatron@gmail.com>
2014-12-29 10:13:14 -0600

    Aguila theme - IE Fixes

--- aguila_theme/js/age-gate.js
+++ aguila_theme/js/age-gate.js
@@ -1,20 +1,22 @@
 (function ($) {
-      var original = Drupal.behaviors.age_checker.attach;
-      Drupal.behaviors.age_checker.attach = function (context, settings)  {
-        original(context, settings);
-        setTimeout(function () {
-            var popup = $('#age_checker_verification_popup');
-            if (popup.is(':visible')) {                
-                popup.addClass("aguila");
-                var cont = $("<div id='age_checker_content' class='agegate_content'></div>");
-                cont.append(popup.children());
-                popup.append( cont.find(".media-thumbnail-frame") );
-                popup.append( cont );
-                var overlay = $('#age_checker_overlay');
-                overlay.append($(".agegate_footer"));
-                
-                $("#age_checker_verification_popup input[type='submit']").before( $(".agegate_pre_submit") );
-            }
-        }, 150);
-      };
+    if(typeof Drupal.behaviors.age_checker != "undefined") {
+        var original = Drupal.behaviors.age_checker.attach;
+        Drupal.behaviors.age_checker.attach = function (context, settings)  {
+            original(context, settings);
+            setTimeout(function () {
+                var popup = $('#age_checker_verification_popup');
+                if (popup.is(':visible')) {                
+                    popup.addClass("aguila");
+                    var cont = $("<div id='age_checker_content' class='agegate_content'></div>");
+                    cont.append(popup.children());
+                    popup.append( cont.find(".media-thumbnail-frame") );
+                    popup.append( cont );
+                    var overlay = $('#age_checker_overlay');
+                    overlay.append($(".agegate_footer"));
+                    
+                    $("#age_checker_verification_popup input[type='submit']").before( $(".agegate_pre_submit") );
+                }
+            }, 150);
+        };
+    }
 })(jQuery);
