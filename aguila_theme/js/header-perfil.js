9dbff5e0b006af5dd5b24a1340ff0bc326514daf
Nefi Urena <nefi.e.urena.salas@accenture.com>
2015-01-07 03:46:50 -0600

    Cerveza Aguila - User Perfil - Web styling.

--- aguila_theme/js/header-perfil.js
+++ aguila_theme/js/header-perfil.js
@@ -12,7 +12,22 @@
         $("<p>¡Hola " + $("#edit-field-first-name-und-0-value").val() + "!</p><p>Bienvenido a la tierra de la alegr&iacute;a</p>").appendTo(".header-form");
         el.find("#edit-field-thumbnail").appendTo(".header-form");
         
-        $("#edit-actions--2").append($("[href='/user/logout']"));
-    });    
+        $("#edit-actions--2").append($("<a href='/user/logout'>Cerrar Sesi&oacute;n</a>"));
+    });
+
+    $.initModule(".profile", function ($, el) {
+        el.prepend("<div class='header-form'></div>");
+        $(".header-form").after("<h2>Datos ingresados</h2>");
+         $("<p>¡Hola " + $(".field-name-field-first-name").find(".field-item").text() + "!</p><p>Bienvenido a la tierra de la alegr&iacute;a</p>").appendTo(".header-form");
+        el.find(".user-picture").appendTo(".header-form");
+
+        el.append($("#edit-actions--2"));
+        el.find("[type='submit']").hide();
+        $("#edit-actions--2").append($(".tabs.primary").find("[href$='edit-profile']"));
+        $("[href$='edit-profile']").text("Editar perfil");
+        $("#edit-actions--2").append($(".tabs.primary").find("[href$='edit']"));
+        $("[href$='edit']").text("Editar cuenta");
+        $("#edit-actions--2").append($("<a href='/user/logout'>Cerrar Sesi&oacute;n</a>"));
+    });        
 
 }(jQuery2));
