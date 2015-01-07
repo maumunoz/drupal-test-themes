7b461c8e08879f905eabac9d6f95c7b6b4f9cf3d
maumunoz <maumunoz@gmail.com>
2015-01-07 02:01:46 -0600

    Fixing sub menus for slidebars

--- sabmiller_base_theme/js/slidebars.js
+++ sabmiller_base_theme/js/slidebars.js
@@ -362,9 +362,14 @@
         });
 
         // Expand sidebar sub menus
-        $( '.sb-slidebar' ).on( 'touchend click', '.menu > .expanded > a', function ( event ) {
-            $(event.currentTarget).parent().toggleClass('open');
-            return false;
+        $( '.sb-slidebar' ).on( 'touchend click', '.menu a', function ( event ) {
+            var parent = $(event.currentTarget).parent();
+            if (parent.hasClass('expanded')) {
+                parent.toggleClass('open');
+                return false;
+            } else {
+                followLink(event, $(this));
+            }
         });
 
         // Close Slidebar via site
