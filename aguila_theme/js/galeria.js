1397ff5b34797979864b5c56e66ff7d9ff370afa
Esteban <matatron@gmail.com>
2015-01-12 15:21:07 -0600

    Aguila theme - mass feedback

--- aguila_theme/js/galeria.js
+++ aguila_theme/js/galeria.js
@@ -6,6 +6,7 @@
                 var anchor = $(this).find('.media-gallery-thumb'),
                     image = anchor.find('img');
                 anchor.attr('data-src', image.attr('src').replace('/media_gallery_thumbnail/', '/media_gallery_large/'));
+                anchor.append($("<span class='overlay'>Ampliar Foto</span>"));
             });
         },
         after: function ($, node) {
