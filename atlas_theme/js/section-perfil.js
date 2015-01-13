09ea4c3f272fa56d77d6b159cf277fd00178a8d4
monicastillo <monica.castillo@accenture.com>
2015-01-13 16:17:50 -0600

    Atlas theme - Section perfil - Update css and js
    Moved .no-padding class styles to global css

--- atlas_theme/js/section-perfil.js
+++ atlas_theme/js/section-perfil.js
@@ -3,8 +3,12 @@
     'use strict';
     $.transform('.vista-perfil-chica', {
         process: function ($, node) {
+            //Add iframe for video below to profile info
             var iframeContainer = node.find('.video-perfil-chica .iframe');
             iframeContainer.append($('<iframe src="'+ iframeContainer.data('src') + '"/>'));
+
+            //Add no-padding class to display the background image properly
+            $('#content-inner').addClass('no-padding');
         }
     });
 }(jQuery2));
