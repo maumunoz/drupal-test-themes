9f67443deb1311b4bd10445bb5f6e5d18051ddd4
Esteban <matatron@gmail.com>
2015-01-26 15:01:04 -0600

    Atlas theme - age gate

--- atlas_theme/js/age-gate.js
+++ atlas_theme/js/age-gate.js
@@ -1,8 +1,3 @@
-function isMobile() {
-  try{ document.createEvent("TouchEvent"); return true; }
-  catch(e){ return false; }
-}
-
 (function ($) {
     if(typeof Drupal.behaviors.age_checker != "undefined") {
         var original = Drupal.behaviors.age_checker.attach;
@@ -26,12 +21,16 @@ function isMobile() {
                         el=jQuery(el);
                         if(el.html()=="") el.remove();
                     });
-                    if( isMobile() )
+                    
+                    var isMobile = (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent));
+                    if( isMobile )
                     {
                         popup.find("input.form-text").each(function(i, el) {
-                           $("<input type='number' />").attr({ name: this.name, placeholder: this.value, id: this.id, size: this.size, maxlength: this.maxlength, "class": this["class"] }).attr("maxlength",$(this).attr("maxlength")).data("i",i+1).keyup(function(){ age_checker.nextbox(this, jQuery(this).data("i") ); }).addClass("whiteplaceholder").insertBefore(this);
+                           $("<input type='tel' />").attr({ name: this.name, placeholder: this.value, id: this.id, size: this.size, maxlength: this.maxlength, "class": this["class"] }).attr("maxlength",$(this).attr("maxlength")).data("i",i+1).keyup(function(){ jQuery(this).val( this.value.substr(0,4) ); age_checker.nextbox(this, jQuery(this).data("i") ); }).attr("max","2999").addClass("whiteplaceholder").insertBefore(this);
                         }).remove();
+                        //document.getElementById('edit-submitted-new-1416608538262-new-1416608591397').setAttribute('pattern',"\\d*");
                     }
+
                     
                     var submit = popup.find("input[type='submit']");
                     submit.bind('click', function () {
