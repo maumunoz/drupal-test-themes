719bcb1dfa69306951e78ac1b6ab8192029be207
jose.montero <jose.montero@accenture.com>
2015-03-24 11:52:06 -0600

    Suprema Theme - Age Gate Banner blinking - Fixed

--- suprema_theme/js/section-age-gate.js
+++ suprema_theme/js/section-age-gate.js
@@ -13,24 +13,25 @@
     $.initModule(node, 200, function ($, el) {
         // Hide module before transformation
         if (jq.cookie(cookieName) === null) {
-            el.hide();
             banner = el.find('.age_checker-banner');
             bgContainer = el.find('.bg-container');
             overlay = el.parent();
 
             // Convert Image to CSS Backgrounds
-            if($(banner).length) {
-                banner.css('backgroundImage', 'url(' + banner.find('img').attr('src') + ')');
-                banner.find('img').remove();
-            }
             if($(bgContainer).length && overlay.css('backgroundImage')) {
                 bgContainer.removeAttr('style');
                 bgContainer.css('backgroundImage', overlay.css('backgroundImage'));
                 overlay.css('backgroundImage', '');
             }
+            if($(banner).length) {
+                setTimeout(function () {
+                    banner.css('backgroundImage', 'url(' + banner.find('img').attr('src') + ')');
+                    banner.css('filter', 'progid:DXImageTransform.Microsoft.AlphaImageLoader(src="' + banner.find('img').attr('src') + '", sizingMethod="scale")');
+                    banner.addClass('animate-hidden');
+                    banner.find('img').remove();
+                }, 3000);
+            }
 
-            // Show module after transformation
-            el.show();
         }
     });
 
