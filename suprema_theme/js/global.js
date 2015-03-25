2491c677af823f86dfdd098cb7375caf22cc0ad8
Esteban <matatron@gmail.com>
2015-03-25 11:01:17 -0600

    Suprema theme - header responsive fixes

--- suprema_theme/js/global.js
+++ suprema_theme/js/global.js
@@ -2,6 +2,7 @@
 
     //Initialize Page content
     $.initModule("#search-block-form", function ($, searchblock) {
+        $(searchblock).before( $(".btn-menu-mobile") );
         $(searchblock).before( $(".social-header-left").parent().children() );
         $(".social-header-left").find('br').remove();
     });
