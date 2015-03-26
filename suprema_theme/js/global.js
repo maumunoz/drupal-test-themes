0d3fb28644ea1e623868f2b2e42f91cb674ba0db
Esteban <matatron@gmail.com>
2015-03-26 11:07:31 -0600

    Suprema theme - home styles - new CSS

--- suprema_theme/js/global.js
+++ suprema_theme/js/global.js
@@ -1,6 +1,21 @@
 (function ($) {
-
+    
     //Initialize Page content
+    $.initModule(".region-content", function ($, pageContent) {
+        //Add css class to page based on URL
+        var contentAttr = pageContent.find("*[typeof*='foaf:Document']"),
+            classList;
+        if(contentAttr.length) {
+            contentAttr = contentAttr.attr('about');
+            if(typeof contentAttr !== typeof undefined && contentAttr !== false) {
+                classList = contentAttr.split( '/' );
+                $.each(classList, function(i, val) {
+                    $('body').addClass(val);
+                });
+            }
+        }
+    });
+
     $.initModule("#search-block-form", function ($, searchblock) {
         $(searchblock).before( $(".btn-menu-mobile") );
         $(searchblock).before( $(".social-header-left").parent().children() );
