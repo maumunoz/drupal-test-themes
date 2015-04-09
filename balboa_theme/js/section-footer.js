d96abcc9c51fab470832dbe97bfc62a939285ae6
jose.montero <jose.montero@accenture.com>
2015-04-09 15:42:35 -0600

    Balboa Theme - Footer styles and transformations

---
+++ balboa_theme/js/section-footer.js
@@ -0,0 +1,20 @@
+/*global jQuery2 */
+(function ($) {
+    'use strict';
+    var node = '#footer';
+
+    $.initModule(node, function ($, footer) {
+        // Make all the footer links which target starts with "http" to open in a new window
+        $(footer).find('a[href^="http"]').attr({target: '_blank'});
+    });
+
+    // Set Footer as a Sticky Footer
+    $(window).on('resize', function() {
+        $('#page .page-width > .lining').css('paddingBottom', $('#footer').outerHeight());
+    });
+    $(document).ready(function () {
+        $(window).resize();
+    });
+
+}(jQuery2));
+
