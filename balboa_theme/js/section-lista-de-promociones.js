8c1535bc31cf1950cb9c86895e04463bbd7aadda
jose.montero <jose.montero@accenture.com>
2015-04-17 14:18:20 -0600

    Balboa Theme - Lista de Promociones - Placeholder elements transformation

---
+++ balboa_theme/js/section-lista-de-promociones.js
@@ -0,0 +1,28 @@
+/*global jQuery2 */
+(function ($) {
+    'use strict';
+    var node = '.lista-promos',
+        offset = 4,
+        items = 0,
+        list,
+        i;
+
+    $.initModule(node, function ($, promoList) {
+        list = promoList.find('.item-list ul');
+        items = (list.find('li').length) ? list.find('li').length : 0;
+
+        if (items > 0) {
+            for (i = offset; i > items; i--) {
+                list.find('li').eq(0).clone().addClass('placeholder').appendTo(list);
+            }
+        }
+
+        $(list).on('click touch', '.placeholder a', function(e) {
+            e.preventDefault();
+            e.stopPropagation();
+        });
+
+    });
+
+}(jQuery2));
+
