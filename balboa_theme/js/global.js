a8e1250388b60879800a6093ce18fb271dc6a580
jose.montero <jose.montero@accenture.com>
2015-04-22 21:38:34 -0600

    Balboa - Global - Adding Css class based on Content title Balboa - Global - Updating hidden selector

--- balboa_theme/js/global.js
+++ balboa_theme/js/global.js
@@ -1,3 +1,25 @@
+/* globals jQuery2 */
 (function ($) {
+'use strict';
+//Initialize Page content
+$.initModule('.region-content', function ($, pageContent) {
+    //Add css class to page based on URL
+    var contentAttr = pageContent.find('*[typeof*="foaf:Document"]'),
+        titleAttr = pageContent.find('*[property*="dc:title"]'),
+        classList;
+    if(contentAttr.length) {
+        contentAttr = contentAttr.attr('about');
+        if(typeof contentAttr !== typeof undefined && contentAttr !== false) {
+            classList = contentAttr.split( '/' );
+            $.each(classList, function(i, val) {
+                $('body').addClass(val);
+            });
+        }
+    }
+    if(titleAttr.length) {
+        titleAttr = 'title-'+titleAttr.attr('content').replace(/\s/g,'-');
+        $('body').addClass(titleAttr);
+    }
+});
 
 })(jQuery2);
