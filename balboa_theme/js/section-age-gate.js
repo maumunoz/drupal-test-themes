245592c79b5ee8b94d0e0170840f698eabe7ae18
Esteban <matatron@gmail.com>
2015-05-08 11:47:59 -0600

    Balboa theme - Age Gate - problemas IE

--- balboa_theme/js/section-age-gate.js
+++ balboa_theme/js/section-age-gate.js
@@ -7,16 +7,25 @@
             month: 'mes(00)',
             year: 'año(0000)'
         };
+    
+    function clearFormStyles(time) {
+        if(!time) time=300;
+        setTimeout( function() {
+            jQuery("#age-checker-getform").attr("style",""); 
+        }, time);
+    }
+    
+    $(window).on('resize', clearFormStyles);
 
     $.initModule(node, function ($, ageGate) {
         // Reset default values
-        $(ageGate).find('.form-item-day input').removeAttr('onblur').removeAttr('onfocus').val(dateTxt.day).attr("pattern","[0-9]*").attr("max","99").attr("maxlength","2").bind("keyup change input",function() {
+        $(ageGate).find('.form-item-day input').removeAttr('onblur').removeAttr('onfocus').val(dateTxt.day).attr("pattern","[0-9]{1,2}").attr("max","99").attr("maxlength","2").bind("keyup change input",function() {
             this.value=this.value.replace(/[\D]+/g,'');
         });
-        $(ageGate).find('.form-item-month input').removeAttr('onblur').removeAttr('onfocus').val(dateTxt.month).attr("pattern","[0-9]*").attr("max","99").attr("maxlength","2").bind("keyup change input",function() {
+        $(ageGate).find('.form-item-month input').removeAttr('onblur').removeAttr('onfocus').val(dateTxt.month).attr("pattern","[0-9]{1,2}").attr("max","99").attr("maxlength","2").bind("keyup change input",function() {
             this.value=this.value.replace(/[\D]+/g,'');
         });
-        $(ageGate).find('.form-item-year input').removeAttr('onblur').removeAttr('onfocus').val(dateTxt.year).attr("pattern","[0-9]*").attr("max","2999").attr("maxlength","4").bind("keyup change input",function() {
+        $(ageGate).find('.form-item-year input').removeAttr('onblur').removeAttr('onfocus').val(dateTxt.year).attr("pattern","[0-9]{4}").attr("max","2999").attr("maxlength","4").bind("keyup change input",function() {
             this.value=this.value.replace(/[\D]+/g,'');
         });
 
@@ -49,6 +58,10 @@
             if ($(this).val() === '') setTextFieldVal($(this), dateTxt.year);
             e.preventDefault();
         });
+        
+        clearFormStyles(1000);
+        clearFormStyles(2000);
+        clearFormStyles(3000);
     });
 
     function setTextFieldVal (el, value) {
